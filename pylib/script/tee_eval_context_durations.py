4a2eb6c964f7e9ddb1df122a21fadda4c7cf7c18
mmore500 <mmore500.login@gmail.com>
2024-12-27 02:47:02 +0000

    Deploying to binder-2024-11-25-graph_comparison_normalized_barchart.ipynb from @ mmore500/hstrat-reconstruction-algo@cec3b2871dfae0b5df035d860f106d24acca3abd 🚀

---
+++ pylib/script/tee_eval_context_durations.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import argparse
+import datetime
+import multiprocessing
+import socket
+import sys
+import typing
+
+import polars as pl
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Tee stdin to stdout and parse timing records.",
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=str,
+        default="context_durations.csv",
+        help="Output file path.",
+    )
+    parser.add_argument(
+        "--with-col",
+        type=list,
+        action="append",
+        default=[],
+        help="Additional columns for data file.",
+    )
+    return parser.parse_args()
+
+
+def tee_lines() -> typing.List[str]:
+    lines = []
+    for line in sys.stdin:
+        lines.append(line)
+        sys.stdout.write(line)
+    return lines
+
+
+def main() -> None:
+    args = parse_args()
+
+    lines = tee_lines()
+    timing_records = {
+        k: v
+        for line in lines
+        if line.startswith("!!!")
+        for k, v in eval(line.strip().removeprefix("!!!")).items()
+    }
+    timing_results = pl.DataFrame(
+        {
+            "what": timing_records.keys(),
+            "duration (s)": timing_records.values(),
+        },
+        schema={
+            "what": str,
+            "duration (s)": float,
+        },
+    ).with_columns(
+        *(eval(col) for col in args.with_col),
+    ).with_columns(
+        cpu_count=pl.lit(multiprocessing.cpu_count()),
+        date=pl.lit(datetime.datetime.now().isoformat()),
+        hostname=pl.lit(socket.gethostname()),
+    )
+
+    if not args.output.endswith(".csv"):
+        raise ValueError("Output file must be a CSV file.")
+
+    timing_results.write_csv(args.output)
+
+
+if __name__ == "__main__":
+    main()
