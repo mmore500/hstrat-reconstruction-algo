2d23c78955492eb1bdc70661795951fbc24436ff
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-08-20 16:48:46 -0400

    Update arxiv.sh

--- script/arxiv.sh
+++ script/arxiv.sh
@@ -5,16 +5,8 @@ shopt -s globstar
 
 for binderpath in binder*; do
     echo "binderpath ${binderpath}"
-    find "${binderpath}" -type f ! \( -name "cnorm=log+num-generations=4096+surface-size=256+viz=site-ingest-depth-by-rank-heatmap+ynorm=linear+ext=.png" -o -name "*viz=site-reservation-by-rank-spliced-at-heatmap+ext=.png" -o -name "*.pdf" -o -name "*.tex" -o -name "*.bib" \) -exec rm -f {} +
+    find "${binderpath}" -type f ! \( -name "*.pdf" -o -name "*.tex" -o -name "*.bib" \) -exec rm -f {} +
 done
-
-rm -rf binder/teeplots/0*
-rm -rf binder/teeplots/{10,21,30,40}
-
-# copy symlinks, see https://stackoverflow.com/a/17882368/17332200
-cp -rL implemented_pseudocode implemented_pseudocode_copy
-rm -rf implemented_pseudocode
-mv implemented_pseudocode_copy implemented_pseudocode
 
 find . -type f -name '*.jpg' -exec rm -f {} +
 find . -type d -name dishtiny -exec rm -rf {} +
@@ -22,6 +14,14 @@ find . -type d -name hstrat -exec rm -rf {} +
 find . -type d -name conduit -exec rm -rf {} +
 find . -type d -name docs -exec rm -rf {} +
 find . -type d -empty -delete
+
+find ./binder -type f -name "*.pdf" -print | while read -r f; do
+    base=$(basename "$f" .pdf)   # strip .pdf extension
+    if ! grep -qF "$base" ./**/*.tex 2>/dev/null; then
+        echo rm "$f"
+        rm "$f"
+    fi
+done
 
 rm -f arxiv.tar.gz
 git checkout bibl.bib
@@ -31,4 +31,5 @@ make clean
 mv bibl.bib main.bib
 cp bu1.bbl main.bbl
 cp bu1.blg main.blg
+rm draft.tex
 tar -czvf arxiv.tar.gz *
