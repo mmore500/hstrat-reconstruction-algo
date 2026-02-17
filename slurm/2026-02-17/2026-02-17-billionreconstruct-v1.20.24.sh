2f320496981719ebc21010cdc465fce8ece39f9b
mmore500 <mmore500@users.noreply.github.com>
2026-02-17 23:59:55 +0000

    Deploying to binder-2024-11-11-build_trie_hstrat_searchtable_data_generation.ipynb from @ mmore500/hstrat-reconstruction-algo@42416dca61c3b90508d8d11d02162529178d5cb1 🚀

--- slurm/2026-02-17/2026-02-17-billionreconstruct-v1.20.24.sh
+++ slurm/2026-02-17/2026-02-17-billionreconstruct-v1.20.24.sh
@@ -417,7 +417,8 @@ for phylo_path in "${BATCHDIR}"/__*/**/a=phylo+ext=.pqt; do
             python3 -m hstrat._auxiliary_lib._alifestd_downsample_tips_polars \
             "\${downsample_outpath}" \
             -n 50000 \
-            --seed 1
+            --seed 1 \
+            --progress
     ls -l "\${downsample_outpath}"
     du -h "\${downsample_outpath}"
 done
